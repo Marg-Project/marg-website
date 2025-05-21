0df1644fd9b02bc753af6d4315dd8414936f3f67
tkalra11 <tkalra1111@gmail.com>
2025-05-21 13:53:00 +0530

    Updated Pages

--- Marg/Marg/settings.py
+++ Marg/Marg/settings.py
@@ -16,12 +16,14 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# SECURITY WARNING: keep the secret key used in production secret!
+import os
+from dotenv import load_dotenv
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y8(5r%sx*63=)$f)zlyc4h8mmapwy52*7-y3j5gzy04-9os_h5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
