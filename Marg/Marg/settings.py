e004667ffb8eb7291f07a22b9419765903b0082c
Marg-Project <projectuse18@gmail.com>
2025-05-26 17:48:20 +0530

    Changes hosts

--- Marg/Marg/settings.py
+++ Marg/Marg/settings.py
@@ -27,8 +27,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+import os
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS').split(',')
 
 
 # Application definition
